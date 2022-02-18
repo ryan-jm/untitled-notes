72fe9ca0c9bb6038599ab1c7c142e19b66f55a52
jdwd40 <jdwd40.tech>
2022-02-18 14:15:51 +0000

    started work on ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 
 import AuthProvider from '../contexts/AuthContext';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <ChakraProvider>
       <Component {...pageProps} />
+    </ChakraProvider>
     </AuthProvider>
   );
 }
