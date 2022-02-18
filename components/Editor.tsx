5c2796faf6c1f5434d60d55e3124b4b7d7e5c656
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-18 12:02:16 +0000

    loads the last created note

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,11 +1,11 @@
 import { EditorComponent, useHelpers } from '@remirror/react';
-import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query, Timestamp, where } from 'firebase/firestore';
 import React from 'react';
 
 import { useAuth } from '../contexts/AuthContext';
 import { db } from '../firebase/clientApp';
 
-const Editor = ({ state }: any) => {
+const Editor = ({ state, manager }: any) => {
   const { user } = useAuth();
   const { getJSON } = useHelpers();
 
@@ -20,10 +20,26 @@ const Editor = ({ state }: any) => {
     addDoc(collectionRef, dbEntry);
   };
 
+  const loadNote = async () => {
+    // Currently loads the most recent note, but queries will remain useful for loading a list of all user-added notes.
+    const totalNotes = [];
+    const loadQuery = query(collection(db, 'notes'), where('user', '==', user.uid));
+    const noteSnapshot = await getDocs(loadQuery);
+    noteSnapshot.forEach((doc) => totalNotes.unshift(doc.data()));
+
+    const doc = {
+      type: 'doc',
+      content: totalNotes[0].content.content,
+    };
+
+    manager.view.updateState(manager.createState({ content: doc }));
+  };
+
   return (
     <>
       <EditorComponent />
       <button onClick={handleSave}>Save</button>
+      <button onClick={loadNote}>Load</button>
     </>
   );
 };
