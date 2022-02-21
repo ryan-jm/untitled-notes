319ced894e25d29fdf146ad772842641d95414fe
Gabriella-dev <gd.gabriella@gmail.com>
2022-02-21 15:51:51 +0000

    try to download file from firebase

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,42 @@
+import { DownloadIcon } from '@chakra-ui/icons';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { storageRef, storage } from '../firebase/clientApp';
+
 export default function Dashboard() {
-  return <h1>Dash Board</h1>;
+  function download() {
+    getDownloadURL(storageRef)
+      .then((url) => {
+        console.log(url);
+
+        // const xhr = new XMLHttpRequest();
+
+        // xhr.responseType = 'blob';
+        // xhr.onload = (event) => {
+        //   const blob = xhr.response;
+        // };
+        // xhr.open('GET', url);
+        // xhr.send();
+
+        //   // Or inserted into an <img> element
+        const img = document.getElementById('image');
+        img.setAttribute('src', url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  const name = ref(storage, 'Name');
+  const sparkyRef = ref(storage, '');
+
+  return (
+    <h1>
+      Dash Board
+      <br />
+      <button id="but" onClick={() => download()}>
+        Download
+      </button>
+      <img src="" id="image" />
+      <br />
+    </h1>
+  );
 }
