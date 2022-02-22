25504706a9b3eceec4e23decb5d42823027d4118
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-22 16:02:17 +0000

    hooks which the hyperlink toolbar rely upon

---
+++ components/Editor/hooks/useLinkShortcut.ts
@@ -0,0 +1,25 @@
+import { useExtension } from '@remirror/react';
+import { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { LinkExtension, ShortcutHandlerProps } from 'remirror/extensions';
+
+const useLinkShortcut = () => {
+  const [shortcut, setShortcut] = useState<ShortcutHandlerProps | undefined>();
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  useExtension(
+    LinkExtension,
+    ({ addHandler }) =>
+      addHandler('onShortcut', (props) => {
+        if (!isEditing) {
+          setIsEditing(true);
+        }
+
+        return setShortcut(props);
+      }),
+    [isEditing]
+  );
+
+  return { shortcut, isEditing, setIsEditing };
+};
+
+export default useLinkShortcut;
