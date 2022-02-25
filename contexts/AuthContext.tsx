05b07f1c0a4dc38ef18eecb487ff5740dc51e85a
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-25 14:09:00 +0000

    Refactor

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,12 +1,14 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { createContext, useContext, useState } from 'react';
+import { collection, limit, orderBy, query, where } from 'firebase/firestore';
+import { createContext, useContext, useEffect, useState } from 'react';
 
-import { auth } from '../firebase/clientApp';
+import { auth, db } from '../firebase/clientApp';
 
 interface IAuthContext {
   user: any;
   login?: () => Promise<void>;
   logout?: () => void;
+  getNotes?: () => any[] | undefined;
 }
 
 const initialState = {
@@ -27,8 +29,6 @@ const AuthProvider = ({ children }: any) => {
       const token = credential?.accessToken;
       const user = res.user;
       setUser(user);
-
-      console.log({ credential, token, user });
     } catch (err) {
       console.log(err);
     }
@@ -37,7 +37,6 @@ const AuthProvider = ({ children }: any) => {
   const logout = () => {
     auth.signOut();
     setUser(null);
-    console.log('Logged out.');
   };
 
   return <UserContext.Provider value={{ user, login, logout }}>{children}</UserContext.Provider>;
