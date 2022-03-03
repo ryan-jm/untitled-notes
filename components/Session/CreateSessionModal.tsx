fa03838d280e9c04f5c1fe2169820d83b7248d88
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-03-03 09:04:26 +0000

    edit initial note setting

--- components/Session/CreateSessionModal.tsx
+++ components/Session/CreateSessionModal.tsx
@@ -50,7 +50,7 @@ const CreateSessionModal = ({ isOpen, setShowModal }) => {
         owner: user.uid,
         owner_display_name: displayName,
         owner_color: colorValue,
-        note: currentNote,
+        note: currentNote.content,
         password: password,
         created_at: Timestamp.fromDate(new Date(Date.now())),
       };
